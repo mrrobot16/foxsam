2dc6f058874a4f8a2150decfee0e88fffa3dad0f
= <hmgaar@gmail.com>
2017-01-13 23:36:31 -0500

    added net monthly and annuall net

--- src/app/paycheck/paycheck.component.ts
+++ src/app/paycheck/paycheck.component.ts
@@ -6,14 +6,15 @@ import CustomValidators from '../forms/CustomValidators';
   styleUrls: ['./paycheck.component.css'],
   template: `
 
-
+  <div id="paycheck-section">
     <div id="results">
-      <p *ngIf="paycheck.gross_pay">Gross Pay: {{paycheckForm.value.gross_pay}}</p>
-      <p *ngIf="paycheck.fed_tax">Federal Income Tax: {{paycheck}}</p>
-      <p *ngIf="paycheck.social_security">Social Security: {{paycheck.social_security}}</p>
-      <p *ngIf="paycheck.medicare">Medicare: {{paycheck}}</p>
-      <p *ngIf="paycheck.annual_net">Annual Net: {{paycheck}}</p>
-      <p *ngIf="paycheck.monthly_net">Monthly Net: {{paycheck}}</p>
+     <h2>Paycheck info</h2>
+      <p>Gross Pay: {{paycheck.gross_pay}}</p>
+      <p>Federal Income Tax: {{paycheck.fed_tax}}</p>
+      <p>Social Security: {{paycheck.social_security}}</p>
+      <p>Medicare: {{paycheck.medicare}}</p>
+      <p>Annual Net: {{paycheck.net_annual}}</p>
+      <p>Monthly Net: {{paycheck.net_monthly }}</p>
     </div>
     <div id="paycheck">
     <h2>Your Paycheck</h2>
@@ -21,7 +22,7 @@ import CustomValidators from '../forms/CustomValidators';
         <div class="paycheck-form">
           <label class="flexify">
             Gross Pay
-            <input type="number" formControlName="gross_pay" [value]="45000" placeholder="How money?" (ngModelChange)="computePaycheck()">
+            <input type="number" formControlName="gross_pay" [value]="45000" placeholder="How money?" (keyup)="computePaycheck()">
           </label>
         <label class="flexify">
             Pay Frequency:
@@ -41,11 +42,12 @@ import CustomValidators from '../forms/CustomValidators';
             </select>
           </label>
           <label class="form-submit">
-            <button type="submit">Calculate</button>
+            <button class="" type="submit">Calculate</button>
           </label>
         </div>
       </form>
     </div>
+  </div>
   `
 })
 export class PaycheckComponent {
@@ -55,20 +57,22 @@ export class PaycheckComponent {
 
   }
   ngOnInit(){
+    this.paycheck.gross_pay = 45000;
     this.paycheckForm = this.formBuilder.group({
-      gross_pay: [0],
+      gross_pay: [45000],
       pay_frequency: [''],
       filling_status: ['']
     });
-
+    this.computePaycheck();
   }
 
   computePaycheck(){
-    console.log("computePaycheck() form object");
+    var paycheck = this.paycheck
     this.paycheck.gross_pay = this.paycheckForm.value.gross_pay;
-    var gross_pay = this.paycheckForm.value.gross_pay;
-    this.paycheck.social_security = gross_pay*0.062;
-    this.paycheck.medicare = gross_pay*0.0145
-
+    this.paycheck.fed_tax = paycheck.gross_pay*0.2;
+    this.paycheck.social_security = paycheck.gross_pay*0.062;
+    this.paycheck.medicare = paycheck.gross_pay*0.0145;
+    this.paycheck.net_annual = paycheck.gross_pay - paycheck.fed_tax;
+    this.paycheck.net_monthly = paycheck.net_annual/12;
   }
 }
